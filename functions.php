f26e3ef59b5b9c986b35cc101e28ca88d544eac2
Varun Kumar Dubey <vapvarun@gmail.com>
2020-01-17 21:31:33 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,34 +17,18 @@ define( 'REIGN_CHILD_THEME_VERSION', '1.0.0' );
 /**
  * Enqueue styles.
  */
-add_action( 'wp_enqueue_scripts', 'enqueue_reign_child_styles', 15 );
-function enqueue_reign_child_styles() {
-	wp_enqueue_style( 'reign-child-css', get_stylesheet_directory_uri() . '/style.css', array( 'reign_style' ), REIGN_CHILD_THEME_VERSION, 'all' );
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
+function child_enqueue_styles() {
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
 }
-
-function reign_get_theme_mod( $name, $default = false ) {
-
-	if ( is_child_theme() )
-		return get_theme_mod( $name, reign_get_parent_theme_mod( $name, $default ) );
-
-	return get_theme_mod( $name, $default );
-}
-
-function reign_get_parent_theme_mods() {
-
-	$slug = get_option( 'template' );
-
-	return get_option( "theme_mods_{$slug}", array() );
-}
-
-function reign_get_parent_theme_mod( $name, $default = false ) {
-	$mods = reign_get_parent_theme_mods();
-
-	if ( isset( $mods[ $name ] ) )
-		return $mods[ $name ];
-
-	if ( is_string( $default ) )
-		$default = sprintf( $default, get_template_directory_uri(), get_stylesheet_directory_uri() );
-
-	return $default;
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+         update_option( 'theme_mods_' . get_template(), $value );
+         return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
 }
