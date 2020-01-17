bc1215bb1d6e7d3673d81246043c614240bbdc67
Varun Kumar Dubey <vapvarun@gmail.com>
2020-01-17 15:19:41 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,3 +21,30 @@ add_action( 'wp_enqueue_scripts', 'enqueue_reign_child_styles', 15 );
 function enqueue_reign_child_styles() {
 	wp_enqueue_style( 'reign-child-css', get_stylesheet_directory_uri() . '/style.css', array( 'reign_style' ), REIGN_CHILD_THEME_VERSION, 'all' );
 }
+
+function reign_get_theme_mod( $name, $default = false ) {
+
+	if ( is_child_theme() )
+		return get_theme_mod( $name, reign_get_parent_theme_mod( $name, $default ) );
+
+	return get_theme_mod( $name, $default );
+}
+
+function reign_get_parent_theme_mods() {
+
+	$slug = get_option( 'template' );
+
+	return get_option( "theme_mods_{$slug}", array() );
+}
+
+function reign_get_parent_theme_mod( $name, $default = false ) {
+	$mods = reign_get_parent_theme_mods();
+
+	if ( isset( $mods[ $name ] ) )
+		return $mods[ $name ];
+
+	if ( is_string( $default ) )
+		$default = sprintf( $default, get_template_directory_uri(), get_stylesheet_directory_uri() );
+
+	return $default;
+}
